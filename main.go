18366fc5d37590475cf5d1974ed87151f3d6a464
Jaipradeesh <jaipradeesh@gmail.com>
2016-08-17 08:05:05 +0530

    Add toml parsing
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- main.go
+++ main.go
@@ -3,22 +3,50 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/BurntSushi/toml"
 	"github.com/julienschmidt/httprouter"
-	"github.com/spf13/viper"
 )
 
-func init() {
-	viper.AddConfigPath(".")
-	viper.SetConfigName("config")
-	_ = viper.ReadInConfig()
+type Config struct {
+	Server struct {
+		port   int
+		logger bool
+	}
+
+	Broker struct {
+		broker            string
+		result_backend    string
+		result_expires_in int
+		exchange          string
+		exchange_type     string
+		default_queue     string
+		binding_key       string
+	}
+}
+
+func ReadConfig() Config {
+	var configfile = "config.toml"
+	_, err := os.Stat(configfile)
+	if err != nil {
+		log.Fatal("Config file is missing: ", configfile)
+	}
+
+	var config Config
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	return config
 }
 
 func main() {
+	var config = ReadConfig()
 	router := httprouter.New()
-	router.GET("/", queuePass)
+	router.GET("/*path", queuePass)
 
-	log.Println("Atlantic server listening at port" + ":" + viper.Get("appPort").(string))
+	log.Println("Atlantic server listening at port"+":", config.server.port)
 
-	log.Fatal(http.ListenAndServe(":"+viper.Get("appPort").(string), middleware(router)))
+	log.Fatal(http.ListenAndServe(":"+config.server.port, middleware(router)))
 }
