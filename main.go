5a9fbd878375cb1ae9097de02b1ccfc5dd96c1e1
Jaipradeesh <jaipradeesh@gmail.com>
2016-09-27 18:19:19 +0530

    Handle websocket frame reception
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- main.go
+++ main.go
@@ -35,13 +35,40 @@ func upgrade_conn(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		log.Println(err)
 	} else {
 		// Establish connection with docker daemon
-		conn_docker, err := net.Dial("unix", "/var/run/docker.sock")
+		_, err := net.Dial("unix", "/var/run/docker.sock")
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Pass connection object to message handler
-		frame_handler(conn_ws, &conn_docker)
+		// Listen for message frames
+		for {
+			message_type, message, err := conn_ws.ReadMessage()
+			if err != nil {
+				// TODO: Pass it onto error handler
+				log.Println(err)
+				break
+			}
+
+			// Validate and parse message frame
+			buffer := new(bytes.Buffer)
+			buffer.ReadFrom(message)
+			requestData := buffer.Bytes()
+
+			if len(requestData) == 0 {
+				// TOOD: Pass it onto error handler
+			} else {
+				bodyParser(requestData, r, w, path)
+			}
+
+			go frame_handler()
+
+			// err = conn_ws.WriteMessage(message_type, message)
+			// if err != nil {
+			// 	// TODO: Pass it onto error handler
+			// 	log.Println(err)
+			// 	break
+			// }
+		}
 	}
 }
 
